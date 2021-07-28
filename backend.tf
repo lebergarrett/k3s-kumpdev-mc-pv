2bfc87249277c2d9b1c32098098e2a3ad4a858a1
imkumpy <lebergarrett@gmail.com>
2021-07-28 02:59:22 +0000

    Modified backend for workspaces

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "kumpdev-terraform-backend"
-    key    = "mc-kumpdev-pvc"
+    key    = "mc-kumpdev-pv/terraform.tfstate"
     region = "us-east-1"
   }
 }
